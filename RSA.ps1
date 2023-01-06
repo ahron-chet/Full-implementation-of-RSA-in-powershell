0b1ad1a709200fea3335a3f601b8d824a30d2ec0
Ahron Chetrit <105120555+ahron-chet@users.noreply.github.com>
2023-01-06 15:55:07 +0200

    Update RSA.ps1

--- RSA.ps1
+++ RSA.ps1
@@ -1,33 +1,9 @@
-function intToByete23($n,$len)
-{
-    $b = @()
-    $l = $len - 1
-    $m = $n
-    for ($i=0; $i-lt$l;$i++)
-    {
-        $c = div256($n)
-
-        if (((power -x 256 -y ($l-$i))*$c) -lt $n)
-        {
-            $n-=[bigint]((power -x 256 -y ($l-$i))*$c)
-            $b += $c
-        }
-        else
-        {
-            $b+=0
-        }
-    }
-    $b = $b + ($m % 256)
-    return $b
-}
-
-
 function pow-mod($x,$y,$z)
 {
-    $n = [bigint]1
-    $x = [bigint]$x
-    $y = [bigint]$y
-    $z = [bigint]$z
+    $n = [System.Numerics.BigInteger]1
+    $x = [System.Numerics.BigInteger]$x
+    $y = [System.Numerics.BigInteger]$y
+    $z = [System.Numerics.BigInteger]$z
     while ($y -gt 0)
     {
         if (-not($y % 2 -eq 0))
@@ -36,6 +12,17 @@ function pow-mod($x,$y,$z)
         }
         $y = $y -shr 1
         $x = $x * $x % $z
+    }
+    return $n
+}
+
+
+function power($x,$y)
+{
+    $n = [System.Numerics.BigInteger]::Parse("1")
+    for ($i=0; $i-lt$y; $i++)
+    {
+        $n=$n*$x
     }
     return $n
 }
@@ -79,19 +66,6 @@ function is-prime($n)
 
 
 
-function power($x,$y)
-{
-    $n = [bigint]1
-    $x = [bigint]$x
-    $y = [bigint]$y
-    for ($i=0; $i-lt$y; $i++)
-    {
-        $n=$n*$x
-    }
-    return $n
-}
-
-
 function div2($n)
 {
     $e = $n-1
@@ -109,10 +83,10 @@ function div2($n)
 
 function iterat($a, $e, $m, $n)
 {
-    $a=[bigint]$a
-    $e=[bigint]$e
-    $m=[bigint]$m
-    $n=[bigint]$n
+    $a=[System.Numerics.BigInteger]$a
+    $e=[System.Numerics.BigInteger]$e
+    $m=[System.Numerics.BigInteger]$m
+    $n=[System.Numerics.BigInteger]$n
     if ((pow-mod -x $a -y $e -z $n) -eq 1)
     {
         return $true
@@ -135,12 +109,12 @@ function iterat($a, $e, $m, $n)
 function miler-rabin($n)
 {
     $em = (div2 -n $n)
-    $e = [bigint]$em[0]
-    $m = [bigint]$em[1]
-    $n = [bigint]$n
+    $e = [System.Numerics.BigInteger]$em[0]
+    $m = [System.Numerics.BigInteger]$em[1]
+    $n = [System.Numerics.BigInteger]$n
     for ($i=0;$i-lt 20; $i++)
     {
-        $a = Get-Random -Minimum 2 -Maximum $n
+        $a = GetRandomRange -min 2 -max $n
         if (-not(iterat -a $a -e $e -m $m -n $n))
         {
             return $false
@@ -149,15 +123,6 @@ function miler-rabin($n)
     return $true
 }
 
-
-
-function random-bit($nbi)
-{
-    $min = [bigint]$nbi -1
-    $max = [bigint]$nbi
-    $r=Get-Random -Minimum ((power -x 2 -y $min)+1) -Maximum ((power -x 2 -y $max)-1)
-    return [bigint]$r+1
-}
 
 
 
@@ -174,12 +139,12 @@ function get-prime2($nbit)
     while ($true)
     {
         $p = (random-bit -nbi $nbit)
-        $p = [bigint]$p
+        $p = [System.Numerics.BigInteger]$p
         $c=0
         foreach($t in $primes)
         {
-            $t = [bigint]$t
-            if ($p % $t -eq [bigint]0)
+            $t = [System.Numerics.BigInteger]$t
+            if ($p % $t -eq [System.Numerics.BigInteger]0)
             {
                 $c=1
                 break
@@ -196,20 +161,27 @@ function get-prime2($nbit)
     }
 }
 
-function round-int($decimel)
-{
-    $t=$decimel.ToString()
-    $res=$t.Split('.')[0]
-    return [bigint]$res
+function GetRandomRange($min,$max){
+    $min = [System.Numerics.BigInteger]::Parse($min)
+    $max = [System.Numerics.BigInteger]::Parse($max)
+    $r = [System.Numerics.BigInteger]::Parse(([System.Numerics.BigInteger](Get-Random -Minimum $min -Maximum $max)))
+    return $r
 }
 
+function random-bit($nbi)
+{
+    $min = [System.Numerics.BigInteger]$nbi -1
+    $max = [System.Numerics.BigInteger]$nbi
+    $r = GetRandomRange -min ((power -x 2 -y $min)+1) -max ((power -x 2 -y $max)-1)
+    return [System.Numerics.BigInteger]::Add($r,1)
+}
 
 
 class Euclids
 {
-    [bigint]gcd ($a,$b)
+    [System.Numerics.BigInteger]gcd ($a,$b)
    {
-        $r=[bigint]0
+        $r=[System.Numerics.BigInteger]0
         while ($true)
         {
             if($a-eq 0 -or $b -eq 0)
@@ -227,129 +199,105 @@ class Euclids
    
    [array]gcdx ($a,$b)
    {
-        #$r=[bigint]0
+        #$r=[System.Numerics.BigInteger]0
         if ($a -eq 0)
         {
             return $b,0,1
         }
-        $r = [bigint] $b%$a
+        $r = [System.Numerics.BigInteger] $b%$a
         $g = [Euclids]::new().gcdx($r,$a)
-        $r = [bigint]$g[0]
-        $x1 = [bigint]$g[1]
-        $y1 = [bigint]$g[2]
-        $x = [bigint]$y1 - (round-int($b/$a)) * $x1
-        $y = [bigint]$x1
+        $r = [System.Numerics.BigInteger]$g[0]
+        $x1 = [System.Numerics.BigInteger]$g[1]
+        $y1 = [System.Numerics.BigInteger]$g[2]
+        $x = [System.Numerics.BigInteger]$y1 - ([System.Numerics.BigInteger]::Divide($b,$a)) * $x1
+        $y = [System.Numerics.BigInteger]$x1
         return $r,$x,$y
    }
 }
 
 
-function intToByetes($n,$len)
+class BitConvert 
 {
-    function div-b($nb)
+    hidden [System.Numerics.BigInteger]div256($n)
     {
-        $nb = [bigint]$n
-        while($nb -gt 256)
-        {
-            $nb = [bigint]$n/[bigint]256
+        while(($n -gt 256 )-or ($n -eq 256)){
+            $n = [System.Numerics.BigInteger]$n/256
         }
-        return [bigint]$nb
+        return [System.Numerics.BigInteger]$n
     }
-    $b = @()
-    $m = $n % 256
-    $nlen=$len-1
-    for($i=0; $i-lt$nlen;$i++)
-    {
-        $c = div-b -nb $n
-        if (((power -x 256 -y ($l-$i))*$c )-lt $n)
-        {
-            $n = $n -((power -x 256 -y ($l-$i))*$c)
-            $b = $b + $c
+
+    [System.Numerics.BigInteger]getBitLen([System.Numerics.BigInteger]$n){
+        $c = 1
+        while(($n -gt 256 )-or ($n -eq 256)){
+            $n = [System.Numerics.BigInteger]::Divide($n,256)
+            $c += 1
         }
-        else
-        {
-            $b = $b + 0
+        return $c
+    }
+
+    [array]intToByete($n,$len){
+        $y = $this.getBitLen($n)
+        if ($y -ne $len){
+            throw [System.Exception] "Number is too large for the specified length."
         }
+        $b = @()
+        $l = $len-1
+        $m = $n
+        for ($i=0; $i-lt$l;$i++)
+        {
+            $c = $this.div256($n)
+            $check = ((power -x 256 -y ($l-$i))*$c)
+            if ($check -lt $n){
+                $n-=$check
+                $b += $c
+            }else{
+                $b += $c % 256
+            }
+        }
+        $b = $b + ($m % 256)
+        return $b
     }
-    $b+=$m
 
-}
-
-
-function len-arr($r)
-{
-    $c=0
-    foreach($i in $r)
+    [System.Numerics.BigInteger]bytesToInt($bytesarray)
     {
-        $c+=1
+        $c = $bytesarray.Length
+        $c = [System.Numerics.BigInteger]$c -1
+        $n = 0
+        for ($i=0;-not($i-gt$c);$i++)
+        {
+            $m = power -x 256 -y ($c-$i)
+            $r = [System.Numerics.BigInteger]::Multiply($m,[System.Numerics.BigInteger]$bytesarray[$i])
+            $n = [System.Numerics.BigInteger]::Add($r,$n)
+        }
+        return [System.Numerics.BigInteger]$n
     }
-    return [bigint]$c
 }
-
-
-
-function bytes-toInt($bytesarray)
-{
-    $c = len-arr($bytesarray)
-    $c = [bigint]$c -1
-    $n = 0
-    for ($i=0;-not($i-gt$c);$i++)
-    {
-        Write-Host($i)
-        $m = power -x 256 -y ($c-$i)
-        $b = [bigint]$bytesarray[$i]
-        $r = [bigint]$m*$b
-        $n += $r
-    }
-    return [bigint]$n
-}
-
-
-
-function str-toBytes($data)
-{
-    $encode = [system.Text.Encoding]::UTF8
-    return $encode.GetBytes($data)
-}
-
-
-function bit-length($n)
-{
-    $c = [bigint]1
-    while ($n -gt 255)
-    {
-        $n = round-int($n/256)
-        $c=$c+[bigint]1
-    }
-    return $c
-}
-
 
 
 class RSA
 {
     [array]genPrivateKey($nbit)
     {
-        $e = [bigint] 65537
+        $e = [System.Numerics.BigInteger] 65537
         $p = get-prime2 -nbit $nbit
         $q = get-prime2 -nbit $nbit
-        $n = [bigint] $q*$p
-        $phi = [bigint]($p-1)*($q-1)
+        $n = [System.Numerics.BigInteger] $q*$p
+        $phi = [System.Numerics.BigInteger]($p-1)*($q-1)
         $x = [Euclids]::new().gcdx($e,$phi)[1]
-        $d = [bigint]$phi+$x
+        $d = [System.Numerics.BigInteger]$phi+$x
         $public = @($e,$n)
         $private = @($e,$p,$q,$n,$d)
         return @($public,$private)  
     }
 
-    [bigint]encrypt($public,$m)
+    [System.Numerics.BigInteger]encrypt($public,$m)
     {
-        $e = [bigint] $public[0]
-        $n = [bigint] $public[1]
+        $e = [System.Numerics.BigInteger] $public[0]
+        $n = [System.Numerics.BigInteger] $public[1]
         $enc = pow-mod -x $m -y $e -z $n
         return $enc
     }
-    [bigint]decrypt($private,$m)
+    [System.Numerics.BigInteger]decrypt($private,$m)
     {
         $d = $private[-1]
         $n = $private[-2]
@@ -358,20 +306,21 @@ class RSA
     }
     [string]encryptData($public,$data)
     {
-        $data = str-toBytes($data)
-        $m = bytes-toInt($data)
+        $con = [BitConvert]::new()
+        $m = $con.bytesToInt($data)
         $enc = [RSA]::new().encrypt($public,$m)
-        $bitlen = bit-length($enc)
-        $enc = intToByetes -n $enc -len $bitlen
+        $bitlen = $con.getBitLen($enc)
+        $enc = $con.intToByete($enc,$bitlen)
         return [System.Convert]::ToBase64String($enc) 
     }
     [string]decryptData($public,$data)
     {
-        $m = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($data))
-        $m = bytes-toInt($m)
+        $con = [BitConvert]::new()
+        $m = [System.Convert]::FromBase64String($data)
+        $m = $con.bytesToInt($m)
         $dec = [RSA]::new().decrypt($public,$m)
-        $bitlen = bit-length($dec)
-        $dec = intToByetes -n $dec -len $bitlen
+        $bitlen = $con.getBitLen($dec)
+        $dec = $con.intToByete($dec,$bitlen)
         return $dec
     }
 
@@ -390,8 +339,8 @@ class RSA
 
     [string]verifySignature($public,$signature,$message)
     {
-        $e = [bigint] $public[0]
-        $n = [bigint] $public[1]
+        $e = [System.Numerics.BigInteger] $public[0]
+        $n = [System.Numerics.BigInteger] $public[1]
         $hasher = [System.Security.Cryptography.HashAlgorithm]::Create('sha256')
         $hash = $hasher.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($message))
         $hash = bytes-toInt($hash)
@@ -404,5 +353,6 @@ class RSA
     }
 
 }
+
 
 
